a1b93c481633beff9ac47ad577e7142bd49a10fa
mast <304737254@qq.com>
2020-07-04 11:46:26 +0800

    第一次提交

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("12123123");
+        System.out.println("12123123BBB");
     }
 }
